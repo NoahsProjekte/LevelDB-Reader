8b25994e014f13a1ec0a1049db19dd34757fa687
Noah <soziales@austriannoah.xyz>
2024-08-31 21:43:25 +0200

    keksdev will fix this :)

--- src/main/java/fun/noah/viewerprojects/LevelDBReader.java
+++ src/main/java/fun/noah/viewerprojects/LevelDBReader.java
@@ -44,7 +44,7 @@ public class LevelDBReader {
     }
 
     public static void main(String[] args) {
-        readLevelDB("");
+        readLevelDB("E:\\noah.fun\\ViewerProjects\\LDBR\\000922.ldb");
     }
 
 }
